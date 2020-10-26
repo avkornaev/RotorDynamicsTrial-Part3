b6c052bb7cea49f99d5d230f8ca8f6698840fc13
avkornaev <70199936+avkornaev@users.noreply.github.com>
2020-10-26 10:31:46 +0300

    Add files via upload

---
+++ equationsOfMotion3.m
@@ -0,0 +1,29 @@
+function [dydt,F1,F2]=equationsOfMotion3(t,y,dyn)
+%Equations of motion for the case 1
+
+%Initialisation
+mass=dyn.mass;
+deltam=dyn.deltam;
+Fext=dyn.Fext;
+g=dyn.g;
+n_rps=dyn.n_rps;
+dX=dyn.dX;
+dV=dyn.dV;
+
+omega=2*pi*n_rps;
+dydt=zeros(4,1);%the right-side parts of the equations
+
+%Fluid film reaction forces are calculatee by an ANN
+%!!!!!!!!!!!Enter your code here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+%[F1,F2]=
+%!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+%The right-side parts of the equations
+%!!!!!!!!!!!Enter your code here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ %dydt(1)=
+ %dydt(2)=
+ %dydt(3)=
+ %dydt(4)=
+%!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ t%print time
+end
